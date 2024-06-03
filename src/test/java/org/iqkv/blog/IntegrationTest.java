89845ab324e948d6a9e64af11769c80cf2391b07
Dima Denysenko <coding@dimdnk.com>
2024-06-03 14:29:17 +0300

    Initial version of blog generated by generator-jhipster@8.5.0

---
+++ src/test/java/org/iqkv/blog/IntegrationTest.java
@@ -0,0 +1,21 @@
+package org.iqkv.blog;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.iqkv.blog.config.AsyncSyncConfiguration;
+import org.iqkv.blog.config.EmbeddedSQL;
+import org.iqkv.blog.config.JacksonConfiguration;
+import org.iqkv.blog.config.TestSecurityConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { BlogApp.class, JacksonConfiguration.class, AsyncSyncConfiguration.class, TestSecurityConfiguration.class })
+@EmbeddedSQL
+public @interface IntegrationTest {
+}
